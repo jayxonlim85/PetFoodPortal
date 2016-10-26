ec5e74e7de7a4802f9c083d9a42c6268fc571b53
sky <skymsdn@hotmail.com>
2016-10-26 18:15:22 +0800

    -add ignore file -add include bootstrap to home -use template to customize the home page
    #1
    #2
    #4
    #5

--- PetFoodPortal/PetFoodPortal/App_Start/BundleConfig.cs
+++ PetFoodPortal/PetFoodPortal/App_Start/BundleConfig.cs
@@ -18,12 +18,24 @@ namespace PetFoodPortal
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/index").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/scrollreveal.js",
+                "~/Scripts/jquery.magnific-popup.js",
+                "~/Scripts/jquery.easing.1.3.js",
+                "~/Scripts/creative.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/font-awesome.css"));
+            bundles.Add(new StyleBundle("~/Content/theme").Include(
+                "~/Content/creative.css",
+                "~/Content/magnific-popup.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
